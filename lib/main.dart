b7f3349629d65989621f96767c81a49d707a17d0
JoshuaLopez2000 <54430949+JoshuaLopez2000@users.noreply.github.com>
2024-11-10 22:40:47 -0600

    Se modificó el font general de la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_unity_widget_example/demon_slayer_unity_screen.dart';
+import 'package:flutter_unity_widget_example/screens/demonSlayer/choose_volume_demon_slayer.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +8,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final Map<String, Widget> imageScreens = {
-    'lib/assets/demonSlayer/demonSlayer01.jpg': DemonSlayerUnityScreen(),
-    'lib/assets/jjk/jjk.jpg': ScreenTwo(),
-    'lib/assets/chainSawMan/chainSawMan01.png': ScreenThree(),
+    'assets/demonSlayer/Kimetsu_no_Yaiba_V1.webp': ChooseVolumeDemonSlayer(),
+    'assets/jjk/jjk.jpg': Placeholder(),
+    'assets/chainSawMan/chainSawMan01.png': Placeholder(),
   };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'MangaFont',
+      ),
       home: Scaffold(
         body: Builder(
           builder: (BuildContext context) {
@@ -24,7 +27,9 @@ class MyApp extends StatelessWidget {
               children: [
                 Text(
                   'Manga AR Experience',
-                  style: TextStyle(),
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
                 ),
                 CarouselSlider(
                   items: imageScreens.entries.map((entry) {
@@ -86,98 +91,6 @@ class MyApp extends StatelessWidget {
           },
         ),
       ),
-    );
-  }
-}
-
-// Pantallas individuales como StatefulWidgets
-
-class ScreenOne extends StatefulWidget {
-  @override
-  _ScreenOneState createState() => _ScreenOneState();
-}
-
-class _ScreenOneState extends State<ScreenOne> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Screen One")),
-      body: Center(child: Text("This is Screen One")),
-    );
-  }
-}
-
-class ScreenTwo extends StatefulWidget {
-  @override
-  _ScreenTwoState createState() => _ScreenTwoState();
-}
-
-class _ScreenTwoState extends State<ScreenTwo> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Screen Two")),
-      body: Center(child: Text("This is Screen Two")),
-    );
-  }
-}
-
-class ScreenThree extends StatefulWidget {
-  @override
-  _ScreenThreeState createState() => _ScreenThreeState();
-}
-
-class _ScreenThreeState extends State<ScreenThree> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Screen Three")),
-      body: Center(child: Text("This is Screen Three")),
-    );
-  }
-}
-
-class ScreenFour extends StatefulWidget {
-  @override
-  _ScreenFourState createState() => _ScreenFourState();
-}
-
-class _ScreenFourState extends State<ScreenFour> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Screen Four")),
-      body: Center(child: Text("This is Screen Four")),
-    );
-  }
-}
-
-class ScreenFive extends StatefulWidget {
-  @override
-  _ScreenFiveState createState() => _ScreenFiveState();
-}
-
-class _ScreenFiveState extends State<ScreenFive> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Screen Five")),
-      body: Center(child: Text("This is Screen Five")),
-    );
-  }
-}
-
-class ScreenSix extends StatefulWidget {
-  @override
-  _ScreenSixState createState() => _ScreenSixState();
-}
-
-class _ScreenSixState extends State<ScreenSix> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Screen Six")),
-      body: Center(child: Text("This is Screen Six")),
     );
   }
 }
